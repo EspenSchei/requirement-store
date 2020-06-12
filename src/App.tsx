bbcee60aea7eec766f6fe013e88ed9d7bd9b1df1
Espen Schei <espen@jpro.no>
2020-06-12 13:14:29 +0200

    Add css background gradient and search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [question, setQuestion] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='wrapper'>
+      <div className='webflow-style-input'>
+        <input
+          type='text'
+          value={question}
+          placeholder='What are you looking for?'
+          onChange={(event) => setQuestion(event.target.value)}
+        />
+      </div>
     </div>
   );
 }
